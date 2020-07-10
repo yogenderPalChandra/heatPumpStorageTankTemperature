a4e13f226ded4742349d9db3003bf1feeb3d451d
Yogender <yogender027mae@gmail.com>
2020-07-10 09:24:10 +0200

    bigdata.h5 + lstmHpNormalater.py

--- lstmHpNormlater.py
+++ lstmHpNormlater.py
@@ -22,8 +22,8 @@ from numpy.random import seed
 seed(123)
 #from tensorflow import set_random_seed
 #set_random_seed(42)
-import tensorflow
-tensorflow.random.set_seed(42)
+#import tensorflow
+#tensorflow.random.set_seed(42)
 
 def load_data():
     path = "./dlOne"
@@ -82,6 +82,8 @@ df_hp = load_dataHP()
 #df3 = pd.DataFrame(df3)
 
 df3 = pd.concat([df_hp.iloc[:, 0], df_hp.iloc[:, 3], df_hp.iloc[:, 4], df], axis =1)
+#df3_dummy = df3.iloc[:,1:]
+#df3_dummy = pd.concat([df3.iloc[:, 4], df3_dummy], axis =1)
 #df3 = pd.DataFrame(df3)
 
 def normalize(X):
@@ -104,8 +106,8 @@ df2 = df3.iloc[:, 1:]
 k = 3
 n_features = 21
 
-epochs=10000
-batch_size=300
+epochs=5000
+batch_size=10000
 
 
 ###############################################
@@ -129,20 +131,27 @@ def prepare_df(df):
     # new_ys = [x[1] for x in new_ys]
     return new_rows, new_ys
 
+'''
 X, y = prepare_df(df3_nrm)
 idxs = [x[0] for x in y]
 y = np.array([np.array(x[1]) for x in y])
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=False)
+'''
 
 Xdf1, ydf1 = prepare_df(df1)
 
 Xdf2, ydf2 = prepare_df(df2)
 
+#Xdf = pd.DataFrame(Xdf1)
+#ydf = pd.DataFrame(y_df2)
+
 #takingX values from df1 and taking y values of df2 
 
 y_df2 = np.array([np.array(x[1]) for x in ydf2])
+#y_df1 = np.array([np.array(x[1]) for x in ydf1])
 
 X_train, X_test, y_train, y_test = train_test_split(Xdf1, y_df2, test_size=0.2, random_state=42, shuffle=False)
+#X_train, X_test, y_train, y_test = train_test_split(Xdf1, y_df1, test_size=0.2, random_state=42, shuffle=False)
 
 #y = np.array([np.array(x[1]) for x in ydf1])
 
@@ -198,6 +207,10 @@ history = model.fit(X_train.reshape(X_train.shape[0], k, n_features),
 pd.DataFrame(history.history).plot()
 plt.show()
 
+model.save('Bigdata.h5')
+from numpy import loadtxt
+from keras.models import load_model
+model = load_model('Bigdata.h5')
 
 ###########################################
 # plot prediction
@@ -208,20 +221,43 @@ yhat=model.predict(X_test.reshape(X_test.shape[0], k, n_features))
 plt.scatter(y_test, yhat)
 plt.show()
 
+'''
+def unscale_1(y_values, scaler):
+   # it is more complex, because y_values here miss the Tamb column
+   # we add artificial Tamb column and remove it again.
+   y_values_with_Tamb_fake = np.array([np.concatenate([row[:-2], np.array([1.0]), row[-2:]]) for row in y_values])
+   res = scaler.inverse_transform(y_values_with_Tamb_fake)
+   return np.array([np.concatenate([row[:-3], row[-2:]]) for row in res])
+
+unscaled = unscale_1(y_test, scaler_df3)
+
+'''
+def addExtraColumnTamb(arr):
+    #y_extraTamb = pd.concat([df.iloc[:, 0], df], axis=1)
+    return np.concatenate((arr[:, 0].reshape(-1,1), arr), axis=1)
+
+yhat_extraTamb_fake = addExtraColumnTamb(yhat)
+ytest_extraTamb_fake = addExtraColumnTamb(y_test)
+    
+    
+
 def unscale(y_values, scaler):
    return scaler.inverse_transform(y_values)
 
-y_pred_unscaled, y_test_unscaled = unscale(yhat, scaler_df3), unscale(y_test, scaler_df3)
+y_pred_unscaled, y_test_unscaled = unscale(yhat_extraTamb_fake, scaler_df3), unscale(ytest_extraTamb_fake, scaler_df3)
+
+y_pred_unscaled = y_pred_unscaled[:, 1:]
+y_test_unscaled = y_test_unscaled[:, 1:]
 
 def plot(arr_y_pred, arr_y_test, orig_df):
-   xdata = orig_df.iloc[4609:, 0]
+   xdata = orig_df.iloc[82945:, 0]
    df_y_pred = pd.DataFrame(arr_y_pred)
    df_y_test = pd.DataFrame(arr_y_test) # arry_y_pred you took here!
    legends_test =['OrgT' + str(i) for i in range (1, 23) ]
    legends_pred =['PrT' + str(i) for i in range (1, 23) ]
    for i, j in zip(df_y_pred, df_y_test):
-       plt.plot(xdata, df_y_pred.iloc[:, i], label = legends_pred[i])
-       #plt.plot(xdata, df_y_test.iloc[:, j], label = legends_test[i])
+       #plt.plot(xdata, df_y_pred.iloc[:, i], label = legends_pred[i])
+       plt.plot(xdata, df_y_test.iloc[:, j], label = legends_test[i])
 
    plt.legend()
    plt.show()
@@ -231,19 +267,25 @@ plt.clf()
 plot(y_pred_unscaled, y_test_unscaled, orig_df)
 plot(yhat, y_test, orig_df)
 
+
+
 print(f"Best val_loss is: {min(history.history['val_loss'])}")
 
 
 
 
+'''
+for i, j in zip(y_pred_unscaled, y_test_unscaled):
+    print (i, j)
+for i, j in zip(pd.DataFrame(y_pred_unscaled), pd.DataFrame(y_test_unscaled)):
+    print (i, j)
+b = b[:, 1:]
+
+b = np.concatenate((b[:, 3].reshape(-1,1), b), axis=1)
 
 
-
-
-
-
-
-
+b_unscale = unscale(b, q) 
+'''
 
 
 
